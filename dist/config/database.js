53ad6a436cbea6189aca59fe80d8bdd2491ec421
RegionalBoss <eduard.karbovanec@gmail.com>
2022-11-09 09:00:13 +0100

    db connection postgresql

--- dist/config/database.js
+++ dist/config/database.js
@@ -1,15 +1,30 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const path_1 = __importDefault(require("path"));
 exports.default = ({ env }) => ({
     connection: {
-        client: 'sqlite',
+        // client: "sqlite",
+        // connection: {
+        //   filename: path.join(
+        //     __dirname,
+        //     "..",
+        //     "..",
+        //     env("DATABASE_FILENAME", ".tmp/data.db")
+        //   ),
+        // },
+        // useNullAsDefault: true,
+        client: "postgres",
         connection: {
-            filename: path_1.default.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+            host: env("DATABASE_HOST", "localhost"),
+            port: env.int("DATABASE_PORT", 5432),
+            database: env("DATABASE_NAME", "bank"),
+            user: env("DATABASE_USERNAME", "postgres"),
+            password: env("DATABASE_PASSWORD", "0000"),
+            schema: env("DATABASE_SCHEMA", "public"),
+            ssl: false,
+            //  {
+            //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+            // },
         },
-        useNullAsDefault: true,
+        debug: false,
     },
 });
