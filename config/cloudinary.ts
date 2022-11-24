2f9331650abb6077afc5ff7bb8f7c320fb8a1c82
RegionalBoss <eduard.karbovanec@gmail.com>
2022-11-24 13:50:57 +0100

    update translations

---
+++ config/cloudinary.ts
@@ -0,0 +1,6 @@
+export default {
+  cloudName: "sazkamobil",
+  username: "sazkamobil.cms@cleverlance.com",
+  apiKey: "345276388795827",
+  apiSecret: "Na3YVnYGaDkDtA90_Qkmnl-VV6M",
+};
---
+++ dist/config/cloudinary.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    cloudName: "sazkamobil",
+    username: "sazkamobil.cms@cleverlance.com",
+    apiKey: "345276388795827",
+    apiSecret: "Na3YVnYGaDkDtA90_Qkmnl-VV6M",
+};
