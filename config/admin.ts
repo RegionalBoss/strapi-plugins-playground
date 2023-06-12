0b34df96cc13f91ec16fd2c900fb14161ee01287
RegionalBoss <eduard.karbovanec@gmail.com>
2023-06-12 12:19:48 +0200

    update build scripts

--- config/admin.ts
+++ config/admin.ts
@@ -2,4 +2,7 @@ export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'ab9929c75b31a1a2cd4a819d50be9132'),
   },
+  apiToken: {
+    salt: env("API_TOKEN_SALT"),
+  },
 });
--- dist/config/admin.js
+++ dist/config/admin.js
@@ -4,4 +4,7 @@ exports.default = ({ env }) => ({
     auth: {
         secret: env('ADMIN_JWT_SECRET', 'ab9929c75b31a1a2cd4a819d50be9132'),
     },
+    apiToken: {
+        salt: env("API_TOKEN_SALT"),
+    },
 });
